5a7077a395efc1c29bec392a689f13c3bf865274
Adrian Chand <adidasiscool12345@gmail.com>
2024-11-04 19:59:55 +1300

    Added calculate grade interface

--- interface/interface.c
+++ interface/interface.c
@@ -10,5 +10,30 @@ void select_option(){
 	printf("2) Calculate overall course weight\n");
 
 	scanf("%d", &choice);
+
+	if(choice == 1){
+		interface_assignment_grade();
+	} else if (choice == 2){
+		interface_course_weight();
+	}
 	printf("option %d selected \n", choice);
 } 
+
+void interface_assignment_grade(){
+	double user_grade, assignment_weight, overall_weight;
+
+	printf("Enter the weighting of the assignment (0.4): \n");
+	scanf("%lf",  &assignment_weight);
+
+	printf("Enter the percentage score of the assignment (0.1) \n");
+	scanf("%lf", &user_grade);
+
+	overall_weight = user_grade * assignment_weight;
+
+	printf("The overall weight of your grade is %.2lf \n", overall_weight);
+}
+
+void interface_course_weight(){
+	printf("Enter the number of assignments in the course: \n");
+
+}
--- interface/interface.h
+++ interface/interface.h
@@ -2,5 +2,7 @@
 #define INTERFACE_H
 
 void select_option();
+void interface_assignment_grade();
+void interface_course_weight();
 
 #endif 
